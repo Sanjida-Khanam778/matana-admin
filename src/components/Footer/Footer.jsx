fcab49d4d538fd2b15efee27df36e1f13abe0bf0
Sanjida Khanam <sanjida.ice.778@gmail.com>
2026-06-28 16:06:24 +0600

    update footer logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import MatanaLogo from "../../assets/images/logoFooter.png";
+import MatanaLogo from "../../assets/images/logoFooter.svg";
 import ListBusinessModal from "../Modals/ListBusinessModal";
 
 export default function Footer() {
